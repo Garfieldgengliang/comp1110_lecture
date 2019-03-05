f804bd38b18ded4cda9a3c79cac5626ed62b37fa
COMP1110 2018 S1 <comp1110-2018s1@anu.edu.au>
2019-03-05 20:32:37 +1100

    J05 ControlFlowIfThenElse, ControlFlowSwitch J06 ControlFlowWhile, ControlFlowFor J07 Methods J08 Person, Objects

---
+++ src/comp1110/lectures/O01/Person.java
@@ -0,0 +1,16 @@
+package comp1110.lectures.O01;
+
+public class Person {
+    private int age;
+    private String name;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", aged " + age;
+    }
+}
